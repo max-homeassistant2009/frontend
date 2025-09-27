d39be081b837e786befd190c0d24de4fa02885e4
maximilian <maximilian.fischer09@icloud.com>
2025-09-27 10:23:07 +0200

    neuer Stand

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import Button from './Button'
 import { Card, Slider } from '@mui/material'
+import testData from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div style={{margin: 0, padding: 0}}>
-        <Card variant="outlined" sx={{m: '100px', p: '100px', border: '3px solid', borderRadius: '25px',}}>Test</Card>
+        <Card variant="outlined" sx={{m: '100px', p: '30px', border: '3px solid', borderRadius: '25px',}}>
+            <h1>Unsere Gruppe</h1>
+            <ul style={{listStyleType: 'none', padding: 0}}>
+                {testData.map(person => (
+                    <li key={person.id}>{person.id}. {person.name}</li>
+                ))}
+            </ul>
+        </Card>
     </div>
   )
 }
--- src/data.js
+++ src/data.js
@@ -1,5 +1,9 @@
 const testData = [
     { id: 1, name: 'Sean' },
-    { id: 2, name: 'Malte' },,
-    { id: 3, name: 'Vivien' },
+    { id: 2, name: 'Malte' },
+    { id: 3, name: 'Maximilian' },
+    { id: 4, name: 'Lina' },
+    { id: 5, name: 'Evelyn' },
 ]
+
+export default testData
